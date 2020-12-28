494ebe4f44d0c3f0c7eec60c1a5b2776ade26dfc
smudgedpasta <smudgedpasta@gmail.com>
2020-12-28 19:56:17 +0000

    Added response for a single question mark

--- main.py
+++ main.py
@@ -71,88 +71,6 @@ async def on_message(message):
                     "Are you going to pester me all day long?",
                     f"""{"".join(y for x in zip(content[::2].lower(), content[1::2].upper()) for y in x if y)}"""
                 ]
-                
-                content = content.lower().strip("?")
-                words = content.split()
-
-                if has_username(content, words, guild.get_member(521926078403575814), "ora"):
-                    responses = [
-                        "Oh, her? That bitch has such an ego, it makes me look like a street beggar.",
-                        "What about my mommy?",
-                        "Don't mention  *M Y  M O M*  to me.",
-                        "Ora? She actually adopted me. Can't say I'm very close to her.",
-                        "What about her?"
-                    ]
-
-                elif has_username(content, words, guild.get_member(156865912631197696), "zei", "pun king"):
-                    responses = [
-                        "Yuck. Zei...",
-                        "What about my mom's loser boyfriend?",
-                        "Oh, Zei? Yeah, he's about my height, weighs less than me, and has lesbian hair.",
-                        "Uhhh...",
-                        "I'm appauled that you mention such a man in front of me.",
-                        "What about him?"
-                    ]
-
-                elif has_username(content, words, guild.get_member(263469402865926144), "chry"):
-                    responses = [
-                        "Chry? Oh, that sword-loving, helmet-faced schnitzel?"
-                    ]
-
-                elif has_username(content, words, guild.get_member(435245956665966633), "jj", "fliss"):
-                    responses = [
-                        "Nobody knows this, but... When he was a child, he aspired to be a cultist leader.",
-                        "Huh? Repeat that again? I lost you at \"Fliss is cool\".",
-                        "Oh, Fliss. Cool guy, I guess.",
-                        "What about him?"
-                    ]
-
-                elif "why" in words or "is" in words or "how" in words or "are" in words:
-                    responses = [
-                        "I don't really care.",
-                        "You're asking ME?",
-                        "I refuse to answer.",
-                        "Oh my Solaris, I think I need brain bleach.",
-                        "I'm sorry.",
-                        "I'm not sorry.",
-                        "Meh.",
-                        "Athiests.",
-                        "Wouldn't know. Check Google."
-                    ]
-
-                elif "can" in content or "would" in content or "does" in content:
-                    responses = [
-                        "Hell yes.",
-                        "Sure, whatever.",
-                        "No. Just no.",
-                        "Do what you want to.",
-                        "Maybe the day pigs fly.",
-                        "Obviously, you bass turd.",
-                        "Psh, please.",
-                        "No, seriously.",
-                        "Are you a sadist?",
-                        "I guess.",
-                        "Why does that even matter?",
-                        "Uhhh, can I have a cookie instead?"
-                    ]
-
-                else:
-                    responses = [
-                        f"How about I ask you a question: {random.choice(opposite_responses)}",
-                        "Interesting.",
-                        "Screw you.",
-                        "Instead of wasting your time pestering me, you should try drinking some organic oil.",
-                        "I think you should run.",
-                        "Come back with some soup and then maybe I'll speak to you.",
-                        "DERE'S A SNAKE IN MAH BOOT!",
-                        "You know what matters more? The fact that Infinite murdered me.",
-                        "That makes me so sad.",
-                        "What is WRONG with you?!",
-                        "Try screaming first.",
-                        "Sorry, I can't think right now. I had too many frogs earlier.",
-                        "Linkin Park - Hit the Floor.",
-                        "Why would you bring that up?"
-                    ]
 
                 portal_emotes = [
                     "<:Sassy:522184015109947392>",
@@ -162,7 +80,99 @@ async def on_message(message):
                     ""
                 ]
 
-                await channel.send(f"{random.choice(responses)} {random.choice(portal_emotes)}")
+                if content == "?":
+                    _responses = [
+                        "Do you think I'm that stupid?",
+                        "Ask an actual question.",
+                        "Congratulations, you can post a question mark."
+                    ]
+
+                    await channel.send(f"{random.choice(_responses)} {random.choice(portal_emotes)}")
+                
+                if content != "?":
+                    content = content.lower().strip("?")
+                    words = content.split()
+
+                    if has_username(content, words, guild.get_member(521926078403575814), "ora"):
+                        responses = [
+                            "Oh, her? That bitch has such an ego, it makes me look like a street beggar.",
+                            "What about my mommy?",
+                            "Don't mention  *M Y  M O M*  to me.",
+                            "Ora? She actually adopted me. Can't say I'm very close to her.",
+                            "What about her?"
+                        ]
+
+                    elif has_username(content, words, guild.get_member(156865912631197696), "zei", "pun king"):
+                        responses = [
+                            "Yuck. Zei...",
+                            "What about my mom's loser boyfriend?",
+                            "Oh, Zei? Yeah, he's about my height, weighs less than me, and has lesbian hair.",
+                            "Uhhh...",
+                            "I'm appauled that you mention such a man in front of me.",
+                            "What about him?"
+                        ]
+
+                    elif has_username(content, words, guild.get_member(263469402865926144), "chry"):
+                        responses = [
+                            "Chry? Oh, that sword-loving, helmet-faced schnitzel?"
+                        ]
+
+                    elif has_username(content, words, guild.get_member(435245956665966633), "jj", "fliss"):
+                        responses = [
+                            "Nobody knows this, but... When he was a child, he aspired to be a cultist leader.",
+                            "Huh? Repeat that again? I lost you at \"Fliss is cool\".",
+                            "Oh, Fliss. Cool guy, I guess.",
+                            "What about him?"
+                        ]
+
+                    elif "why" in words or "is" in words or "how" in words or "are" in words:
+                        responses = [
+                            "I don't really care.",
+                            "You're asking ME?",
+                            "I refuse to answer.",
+                            "Oh my Solaris, I think I need brain bleach.",
+                            "I'm sorry.",
+                            "I'm not sorry.",
+                            "Meh.",
+                            "Athiests.",
+                            "Wouldn't know. Check Google."
+                        ]
+
+                    elif "can" in content or "would" in content or "does" in content:
+                        responses = [
+                            "Hell yes.",
+                            "Sure, whatever.",
+                            "No. Just no.",
+                            "Do what you want to.",
+                            "Maybe the day pigs fly.",
+                            "Obviously, you bass turd.",
+                            "Psh, please.",
+                            "No, seriously.",
+                            "Are you a sadist?",
+                            "I guess.",
+                            "Why does that even matter?",
+                            "Uhhh, can I have a cookie instead?"
+                        ]
+
+                    else:
+                        responses = [
+                            f"How about I ask you a question: {random.choice(opposite_responses)}",
+                            "Interesting.",
+                            "Screw you.",
+                            "Instead of wasting your time pestering me, you should try drinking some organic oil.",
+                            "I think you should run.",
+                            "Come back with some soup and then maybe I'll speak to you.",
+                            "DERE'S A SNAKE IN MAH BOOT!",
+                            "You know what matters more? The fact that Infinite murdered me.",
+                            "That makes me so sad.",
+                            "What is WRONG with you?!",
+                            "Try screaming first.",
+                            "Sorry, I can't think right now. I had too many frogs earlier.",
+                            "Linkin Park - Hit the Floor.",
+                            "Why would you bring that up?"
+                        ]
+
+                    await channel.send(f"{random.choice(responses)} {random.choice(portal_emotes)}")
     except:
         print(traceback.format_exc(), end="")
 
