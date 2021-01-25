05b1c8bdcf8445911794d1798fcdb470b6d7284f
smudgedpasta <smudgedpasta@gmail.com>
2021-01-25 06:46:22 +0000

    Whoops x2 I am super tired lol

--- main.py
+++ main.py
@@ -106,7 +106,7 @@ async def on_message(message):
                     ""
                 ]
 
-                if set(message.content) == {"?"}
+                if set(message.content) == {"?"}:
                     _responses = [
                         "Do you think I'm that stupid?",
                         "Ask an actual question.",
